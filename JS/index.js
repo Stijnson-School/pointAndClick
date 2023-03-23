fa07796298eff9eecc357bd3a65a3842de11be3d
Stijnson <zakelijk.stijnson@gmail.com>
2023-03-23 11:47:26 +0100

    Update v3

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
 const mainCharacter = document.getElementById("mainCharacter");
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
 const gameWindow = document.getElementById("clickMap");
 const wall1 = document.getElementById("wall-1");
 
@@ -11,4 +12,26 @@ gameWindow.onclick = function(e){
     var y = e.clientY - rect.top - offsetCharacter;
     mainCharacter.style.left = `${x}px`;
     mainCharacter.style.top = `${y}px`;
+
+    switch (e.target.id) {
+        case "tv-1":
+            showSpeech("This is a beautiful TV!");
+            break;
+
+        default:
+            hideSpeech()
+            break;
+    }
 }
+
+function showSpeech(dialog)
+{
+    mainCharacterSpeech.style.opacity = 1;
+    mainCharacterSpeech.innerHTML = dialog;
+}
+
+ function hideSpeech() 
+ {
+    mainCharacterSpeech.style.opacity = 0;
+    mainCharacterSpeech.innerHTML = "...";
+ }
