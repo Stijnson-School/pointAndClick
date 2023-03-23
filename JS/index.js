728fb51b22302dbb9304900854db3d56d6b628e0
Stijnson <zakelijk.stijnson@gmail.com>
2023-03-23 14:06:29 +0100

    update v5

--- JS/index.js
+++ JS/index.js
@@ -16,6 +16,8 @@ gameWindow.onclick = function(e){
 
     switch (e.target.id) {
         case "tv":
+            audioTrack.pause();
+            audioTrack.currentTime = 0
             showSpeech("This is a beautiful TV!");
             break;
 
@@ -24,12 +26,63 @@ gameWindow.onclick = function(e){
             audioTrack.play();
             break;
 
+        case "up-stairs":
+            showSpeech("Lets go upstairs!");
+            audioTrack.pause();
+            audioTrack.currentTime = 0
+            mainCharacter.style.left = "650px";
+            mainCharacter.style.top = "390px";
+            setTimeout(function() {
+                mainCharacter.style.top = "175px";
+                var img =  document.getElementById("tileMap")
+                setTimeout(function() {
+                    img.innerHTML = `<img class="notDragable" ïd="tileMapImg" src="./IMG/pointandclick-tilemap-upstairs.png" alt="tileMap1">`
+                    document.getElementById("clickMap-3").style.display = "none";
+                    
+                    document.getElementById("downstairs").style.display = "none";
+                    document.getElementById("upstairs").style.display = "block";
+
+                    setTimeout(function() {
+                        hideSpeech() 
+                    },2000);
+                },500);
+            },1500);
+              break;
+
+        case "down-stairs":
+            showSpeech("Lets go downstairs!");
+            audioTrack.pause();
+            audioTrack.currentTime = 0
+            mainCharacter.style.left = "650px";
+            mainCharacter.style.top = "175px";
+            setTimeout(function() {
+                mainCharacter.style.top = "390px";
+                var img =  document.getElementById("tileMap")
+                setTimeout(function() {
+                    img.innerHTML = `<img class="notDragable" ïd="tileMapImg" src="./IMG/pointandclick-tilemap.png" alt="tileMap1">`
+                    document.getElementById("clickMap-3").style.display = "block";
+                    
+                    document.getElementById("downstairs").style.display = "block";
+                    document.getElementById("upstairs").style.display = "none";
+
+                    setTimeout(function() {
+                        hideSpeech() 
+                    },2000);
+                },500);
+            },1500);
+              break;
+
         default:
             audioTrack.pause();
             audioTrack.currentTime = 0
             hideSpeech()
             break;
     }
+}
+
+function switchImage()
+{
+    
 }
 
 function showSpeech(dialog)
