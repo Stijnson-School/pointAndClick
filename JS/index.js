00c9dc03909b9196dcdbfefc667456c24d0be3e7
Stijnson <zakelijk.stijnson@gmail.com>
2023-03-30 11:44:13 +0200

    .

--- JS/index.js
+++ JS/index.js
@@ -13,11 +13,10 @@ gameWindow.onclick = function(e){
     var y = e.clientY - rect.top - offsetCharacter;
     mainCharacter.style.left = `${x}px`;
     mainCharacter.style.top = `${y}px`;
-
+    audioTrack.pause();
+    audioTrack.currentTime = 0
     switch (e.target.id) {
         case "tv":
-            audioTrack.pause();
-            audioTrack.currentTime = 0
             showSpeech("This is a beautiful TV!");
             break;
 
@@ -28,8 +27,6 @@ gameWindow.onclick = function(e){
 
         case "up-stairs":
             showSpeech("Lets go upstairs!");
-            audioTrack.pause();
-            audioTrack.currentTime = 0
             mainCharacter.style.left = "650px";
             mainCharacter.style.top = "390px";
             setTimeout(function() {
@@ -42,6 +39,9 @@ gameWindow.onclick = function(e){
                     document.getElementById("downstairs").style.display = "none";
                     document.getElementById("upstairs").style.display = "block";
 
+                    document.getElementById("items-downstairs").style.display = "none";
+                    document.getElementById("items-upstairs").style.display = "block";
+
                     setTimeout(function() {
                         hideSpeech() 
                     },2000);
@@ -51,8 +51,6 @@ gameWindow.onclick = function(e){
 
         case "down-stairs":
             showSpeech("Lets go downstairs!");
-            audioTrack.pause();
-            audioTrack.currentTime = 0
             mainCharacter.style.left = "650px";
             mainCharacter.style.top = "175px";
             setTimeout(function() {
@@ -65,12 +63,44 @@ gameWindow.onclick = function(e){
                     document.getElementById("downstairs").style.display = "block";
                     document.getElementById("upstairs").style.display = "none";
 
+                    document.getElementById("items-downstairs").style.display = "block";
+                    document.getElementById("items-upstairs").style.display = "none";
+
                     setTimeout(function() {
                         hideSpeech() 
                     },2000);
                 },500);
             },1500);
               break;
+
+
+        case "item-appleClick":
+            showSpeech("OMG AN APPLE!");
+            mainCharacter.style.left = "150px";
+            mainCharacter.style.top = "190px";
+            setTimeout(function() {
+                hideSpeech()
+            },3000)
+            break;
+
+        case "item-appleClick2":
+            showSpeech("OMG AN APPLE!");
+            mainCharacter.style.left = "435px";
+            mainCharacter.style.top = "220px";
+            setTimeout(function() {
+                hideSpeech()
+            },3000)
+            break;
+
+         case "item-strawberryClick":
+            showSpeech("HMMM A STRAWBERRY!");
+            mainCharacter.style.left = "76px";
+            mainCharacter.style.top = "142px";
+            setTimeout(function() {
+                hideSpeech()
+            },3000)
+            break;
+
 
         default:
             audioTrack.pause();
@@ -80,7 +110,36 @@ gameWindow.onclick = function(e){
     }
 }
 
-function switchImage()
+
+var inventory = {
+    consumables: {
+        apple: [0],
+        strawberry: [0],
+    },
+}
+
+function checkInventory(item)
+{
+    document.getElementById(item + "-count").innerText=parseInt(inventory.consumables[item]);
+
+    if (inventory.consumables[item] > 0){
+        document.getElementById(item).style.display = "block"
+    } else {document.getElementById(item).style.display = "none"}
+}
+
+function addInvItem(item, itemId)
+{
+    setTimeout(function() {
+        inventory.consumables[item] ++;
+        checkInventory(item)
+        document.getElementById(itemId).style.display = "none";
+    },1 * 1000)
+    setTimeout(function() {
+        document.getElementById(itemId).style.display = "block";
+    },120 * 1000)
+}
+
+function removeInvItem()
 {
     
 }
